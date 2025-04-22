0bd7b735e5f3ded68ece68a208ee8aef2a5d7771
Assics99 <razvozzaevivan@gmail.com>
2025-04-22 18:30:06 +0900

    Update python_lesson7.py

--- python_lesson7.py
+++ python_lesson7.py
@@ -2,7 +2,7 @@ import ptbot
 import os
 import random
 
-
+from decouple import config
 from pytimeparse import parse
 from dotenv import load_dotenv
 
@@ -16,32 +16,35 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='
     return '{0} |{1}| {2}% {3}'.format(prefix, pbar, percent, suffix)
 
 
-def reply(chat_id, message):
+def reply(chat_id, message, bot):
     sec = parse(message)
-    message_id = BOT.send_message(chat_id, "Запускаю таймер!")
-    BOT.create_countdown(sec, notify_progress, chat_id=chat_id, message_id=message_id, sec = sec)
-    BOT.create_timer(sec, timer, chat_id=chat_id, message=message)
+    message_id = bot.send_message(chat_id, "Запускаю таймер!")
+    bot.create_countdown(sec, notify_progress, chat_id=chat_id, message_id=message_id, sec = sec, bot = bot)
+    bot.create_timer(sec, timer, chat_id=chat_id, message=message, bot = bot)
 
 
-def notify_progress(secs_left,chat_id, message_id, sec):
+def notify_progress(secs_left,chat_id, message_id, sec, bot):
     progressbar = render_progressbar(sec, sec - secs_left)
     message = "Осталось секунд: {}".format(secs_left)
     display = f'{message}\n{progressbar}'
-    BOT.update_message(chat_id, message_id, display)
+    bot.update_message(chat_id, message_id, display)
 
 
-def timer(chat_id, message):
+def timer(chat_id, message, bot):
     message = "Время вышло!"
-    BOT.send_message(chat_id, message)
+    bot.send_message(chat_id, message)
 
 
 def main():
     load_dotenv()
-    TG_TOKEN = os.environ['TG_TOKEN']
-    global BOT 
-    BOT = ptbot.Bot(TG_TOKEN)
-    BOT.reply_on_message(reply)
-    BOT.run_bot()
+    tg_token = os.environ['tg_token'] 
+    bot = ptbot.Bot(tg_token)
+    bot.reply_on_message(reply, bot = bot)
+    bot.run_bot()
+
+
+if __name__ == '__main__':
+    main()
 
 
 if __name__ == '__main__':
